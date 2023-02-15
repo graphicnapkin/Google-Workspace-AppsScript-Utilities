dab79df5dacba799b393cdb35e8b6acdd52d267f
Jon Carter <graphicnapkin>
2023-02-14 23:03:26 -0500

    Some experimentation to understand how to interact in the react app, with the AppsScript functions. Key takeaway is, outside of a function to get an oauth token, other AppsScript Apis should be moved to a new project, and deployed as a API executable, authed using the token. Additonally you should use api calls directly in the react app if you don't need authorization on the API. Although writing this I'm realizing that I havn't tested an API call nativly from inside the react app.... thats tomorrow Jon's problem I think

--- AppsScriptReactApp/apps-script/main.ts
+++ AppsScriptReactApp/apps-script/main.ts
@@ -3,3 +3,20 @@ function doGet() {
         .evaluate()
         .addMetaTag('viewport', 'width=device-width, initial-scale=1.0')
 }
+
+function getPerson() {
+    return AdminDirectory.Users?.get('jc@graphicnapkin.com').name
+}
+
+function getToken() {
+    return ScriptApp.getOAuthToken()
+}
+
+function getSessionUser() {
+    return Session.getEffectiveUser().getEmail()
+}
+
+function addNumbers(num: number) {
+    console.log(num)
+    return num * 2
+}
--- AppsScriptReactApp/src/App.tsx
+++ AppsScriptReactApp/src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import About from './components/About'
 import Home from './components/Home'
 import Nav from './components/Nav'
 
 function App() {
+    let [first, setFirst] = useState<number | undefined>(4)
+    // you would think this stores a string but NOPE
+    // it stores the result of the Apps Script function
+    // being called.
+    // I think whats happening here contents in between <? ?>
+    // is only being evaluated during the first render from
+    // doGet function, which means you won't be able to dynamically
+    // interact with them. I think the only really solution would be to
+    // move your apps script 'api' endpoints into a new script and
+    // deploy them as a API executable. You can use the getToken pattern
+    // below to call this API executable inside the react app.
+    const addNumberString = '<?= addNumbers(9) ?>'
+    console.log('welp', addNumberString)
+
+    const oAuthToken = '<?= getToken() ?>'
     return (
         <>
             <Nav />
@@ -13,6 +28,23 @@ function App() {
                 <Route path="about" element={<About />} />
                 <Route path="*" element={<Home />} />
             </Routes>
+            <p>Current User: {'<?= getPerson() ?>'}</p>
+            <p>Token: {oAuthToken}</p>
+            <p>Effective User: {'<?= getSessionUser() ?>'}</p>
+            <input
+                value={first}
+                type="input"
+                onChange={(e) => setFirst(parseInt(e.target.value))}
+            />
+            {/* This does not work, will get NaN */}
+            <p>
+                Use Arguments from State Inline:{' '}
+                {'<?= addNumbers(' + first + ') ?>'}
+            </p>
+            {/* This WILL work */}
+            <p>
+                Use Arguments statically set in react: {'<?= addNumbers(9) ?>'}
+            </p>
         </>
     )
 }
